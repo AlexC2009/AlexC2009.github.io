67c8fb5769a1b75ca0d1c639f2d781780ed019e6
AlexC2009 <120340417+AlexC2009@users.noreply.github.com>
2024-11-28 20:05:24 -0800

    Outputs Array and Allowed Actions Conditional

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 var input;
 var output = [];
 function parseInput(){
-    if(input == "North"){
-        document.getElementById("outputLog").innerHTML = document.getElementById("outputLog").innerHTML + "\n North";  
-    } else if(input == "South"){
-        document.getElementById("outputLog").innerHTML = document.getElementById("outputLog").innerHTML + "\n South";  
-    } else if(input == "East"){
-        document.getElementById("outputLog").innerHTML = document.getElementById("outputLog").innerHTML + "\n East";  
-    } else if(input == "West"){
-        document.getElementById("outputLog").innerHTML = document.getElementById("outputLog").innerHTML + "\n West";  
-    } else {
-        document.getElementById("outputLog").innerHTML = "Huh?";
+    //If the action is possible
+    if (true){
+        //Add input to the end of the running list of outputs
+        output.push(input);
+        //Do the action corresponding to the input
+        if(input == "North"){
+            console.log("Action!");
+        } else if(input == "South"){
+            console.log("Action!");
+        } else if(input == "East"){
+            console.log("Action!");
+        } else if(input == "West"){
+            console.log("Action!");
+        }
+        //Add the parsed input to the outputLog
+        //Temporary
+        document.getElementById("outputLog").innerHTML = document.getElementById("outputLog").innerHTML + "\n" + output[output.length-1];
+    } 
+    else{
+        //Otherwise, output an error message
+        document.getElementById("outputLog").innerHTML = document.getElementById("outputLog").innerHTML + "\n Error";
     }
 }
