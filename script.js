f5e3089ce16ccb06c910e3193ec104a17757cbf1
AlexC2009 <120340417+AlexC2009@users.noreply.github.com>
2024-11-29 12:07:33 -0800

    Added diagonal directions

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var input;
 var output = [];
 
 //Other variables
-var movements = ["North", "South", "East", "West", "Up", "Down"];
+var movements = ["North", "South", "East", "West", "Up", "Down", "Northeast", "Northwest", "Southeast", "Southwest"];
 
 //Basic gameplay objects as constructor functions
 function Room(){
@@ -16,6 +16,21 @@ function Room(){
     this.west = false;
     this.up = false;
     this.down = false;
+    this.northEast = false;
+    this.northWest = false;
+    this.southEast = false;
+    this.southWest = false;
+    //Blockages (Doors or otherwise)
+    this.northDoor = false;
+    this.southDoor = false;
+    this.eastDoor = false;
+    this.westDoor = false;
+    this.upDoor = false;
+    this.downDoor = false;
+    this.northEastDoor = false;
+    this.northWestDoor = false;
+    this.southEastDoor = false;
+    this.southWestDoor = false;
 }
 function Level(){
     this.rooms = [];
@@ -38,6 +53,14 @@ var Player = {
             this.atRoom = this.atRoom.up;
         } else if (direction == "Down"){
             this.atRoom = this.atRoom.down;
+        } else if (direction == "Northeast"){
+            this.atRoom = this.atRoom.northeast;
+        } else if (direction == "Northwest"){
+            this.atRoom = this.atRoom.northWest;
+        } else if (direction == "Southeast"){
+            this.atRoom = this.atRoom.southEast;
+        } else if (direction == "Southwest"){
+            this.atRoom = this.atRoom.southWest;
         }
         else {
             //Teleport to specified room if not a direction
@@ -55,10 +78,15 @@ var Player = {
 var L1 = new Level();
 var L1R1 = new Room();
 var L1R2 = new Room();
+var L1R3 = new Room();
 L1R1.description = "Room One";
 L1R1.north = L1R2;
 L1R2.description = "Room Two";
 L1R2.south = L1R1;
+L1R2.down = L1R3;
+L1R3.description = "Room Three";
+L1R3.up = L1R2;
+L1R3.southWest = L1R1;
 L1.rooms = [L1R1, L1R2];
 //Spawn in at l1r1
 Player.atLevel = L1;
@@ -67,17 +95,25 @@ Player.atRoom = L1R1;
 
 function canDo(action){
     //If the action is possible, canDo is true
-    if(action == "North" && Player.atRoom.north != false){
+    if(action == "North" && Player.atRoom.north != false && Player.atRoom.northDoor == false){
         return true;
-    } else if(action == "South" && Player.atRoom.south != false){
+    } else if(action == "South" && Player.atRoom.south != false && Player.atRoom.southDoor == false){
         return true;
-    } else if(action == "East" && Player.atRoom.east != false){
+    } else if(action == "East" && Player.atRoom.east != false && Player.atRoom.eastDoor == false){
         return true;
-    } else if(action == "West" && Player.atRoom.west != false){
+    } else if(action == "West" && Player.atRoom.west != false && Player.atRoom.westDoor == false){
         return true;
-    } else if(action == "Up" && Player.atRoom.up != false){
+    } else if(action == "Up" && Player.atRoom.up != false && Player.atRoom.upDoor == false){
         return true;
-    } else if(action == "Down" && Player.atRoom.down != false){
+    } else if(action == "Down" && Player.atRoom.down != false && Player.atRoom.downDoor == false){
+        return true;
+    } else if(action == "Northeast" && Player.atRoom.northEast != false && Player.atRoom.northEastDoor == false){
+        return true;
+    } else if(action == "Northwest" && Player.atRoom.northWest != false && Player.atRoom.northWestDoor == false){
+        return true;
+    } else if(action == "Southeast" && Player.atRoom.southEast != false && Player.atRoom.southEastDoor == false){
+        return true;
+    } else if(action == "Southwest" && Player.atRoom.southWest != false && Player.atRoom.southWestDoor == false){
         return true;
     } else{
         return false;
@@ -88,18 +124,8 @@ function parseInput(){
         //Add input to the end of the running list of outputs
         output.push(input);
         //Do the action corresponding to the input
-        if(input == "North"){
-            Player.move("North");
-        } else if(input == "South"){
-            Player.move("South");
-        } else if(input == "East"){
-            Player.move("East");
-        } else if(input == "West"){
-            Player.move("West");
-        } else if(input == "Up"){
-            Player.move("Up");
-        } else if(input == "Down"){
-            Player.move("Down");
+        if(movements.includes(input)){
+            Player.move(input);
         }
         //Output the result of the action
         document.getElementById("outputLog").innerHTML = document.getElementById("outputLog").innerHTML + "\n" + output[output.length-1];
