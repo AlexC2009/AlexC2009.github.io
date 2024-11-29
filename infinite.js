c70819f81c11a43182d7a565f4413eb466411235
AlexC2009 <120340417+AlexC2009@users.noreply.github.com>
2024-11-29 14:42:01 -0800

    Testing eval()

---
+++ infinite.js
@@ -0,0 +1,13 @@
+function typeObject(){
+    this.myProperty = 1;
+    this.myMethod = function(){
+        console.log("Method!");
+    }
+}
+
+//The eval() operator takes stuff and evaluates it as code
+for(var i = 0, i < 10, i++){
+    eval("var value" + i + "= new typeObject()")
+}
+
+value9.myMethod;
