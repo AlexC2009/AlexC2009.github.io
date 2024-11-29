49d95839338d6808322fe7608806de823d59b941
AlexC2009 <120340417+AlexC2009@users.noreply.github.com>
2024-11-28 19:19:19 -0800

    Multiple input test

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-var input = 0;
+var input;
+var output = [];
 function parseInput(){
-    document.getElementById("outputLog").innerHTML = "Parsed";
+    if(input = "North"){
+        document.getElementById("outputLog").innerHTML = document.getElementById("outputLog").innerHTML + "\n North";  
+    } else if(input = "South"){
+        document.getElementById("outputLog").innerHTML = document.getElementById("outputLog").innerHTML + "\n South";  
+    } else if(input = "East"){
+        document.getElementById("outputLog").innerHTML = document.getElementById("outputLog").innerHTML + "\n East";  
+    } else if(input = "West"){
+        document.getElementById("outputLog").innerHTML = document.getElementById("outputLog").innerHTML + "\n West";  
+    } else {
+        document.getElementById("outputLog").innerHTML = "Huh?";
+    }
 }
