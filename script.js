d35db9555d52eddc9684749103baea1e3b174f39
AlexC2009 <120340417+AlexC2009@users.noreply.github.com>
2024-11-28 19:24:57 -0800

    Bug fixes (= vs ==)

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 var input;
 var output = [];
 function parseInput(){
-    if(input = "North"){
+    if(input == "North"){
         document.getElementById("outputLog").innerHTML = document.getElementById("outputLog").innerHTML + "\n North";  
-    } else if(input = "South"){
+    } else if(input == "South"){
         document.getElementById("outputLog").innerHTML = document.getElementById("outputLog").innerHTML + "\n South";  
-    } else if(input = "East"){
+    } else if(input == "East"){
         document.getElementById("outputLog").innerHTML = document.getElementById("outputLog").innerHTML + "\n East";  
-    } else if(input = "West"){
+    } else if(input == "West"){
         document.getElementById("outputLog").innerHTML = document.getElementById("outputLog").innerHTML + "\n West";  
     } else {
         document.getElementById("outputLog").innerHTML = "Huh?";
